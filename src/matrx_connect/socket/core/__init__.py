f62129823afb428a34b40299e6f910e1615956e8
Jatin Banga <jatin.b.rx3@gmail.com>
2025-07-01 23:32:32 +0530

    updated inits

--- src/matrx_connect/socket/core/__init__.py
+++ src/matrx_connect/socket/core/__init__.py
@@ -1,5 +1,5 @@
-from .app_factory import get_app_factory
+from .app_factory import get_app_factory, configure_factory
 from .request_base import SocketRequestBase
 from .service_base import SocketServiceBase
 
-__all__ = ["get_app_factory", "SocketRequestBase", "SocketServiceBase"]
+__all__ = ["get_app_factory", "configure_factory", "SocketRequestBase", "SocketServiceBase"]
